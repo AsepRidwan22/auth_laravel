98d620590a11c05edb1d3fed19fde3164eab46ab
AsepRidwan22 <aszeprydzone@gmail.com>
2021-04-06 14:47:52 -0700

    contoh auth

---
+++ resources/views/admin/home.blade.php
@@ -0,0 +1,12 @@
+@extends('adminlte::page')
+
+@section('title', 'Main page')
+
+@section('content_header')
+    <h1>Dashboard</h1>
+@stop
+
+@section('content')
+    <p>Welcome hello to adminlte</p>
+    <p>You are login as Admin</p>
+@stop
---
+++ resources/views/main.blade.php
@@ -0,0 +1,11 @@
+@extends('adminlte::page')
+
+@section('title', 'Main page')
+
+@section('content_header')
+    <h1>Dashboard</h1>
+@stop
+
+@section('content')
+    <p>Welcome hello to adminlte</p>
+@stop
